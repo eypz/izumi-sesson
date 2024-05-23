cc7f6210898ee6f6c9b60096e1aaae201766157d
Eypz God <168025354+eypz@users.noreply.github.com>
2024-05-23 12:11:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb://mongo:OdVjowCFPlSajXDUVruNZBBVyOvBuWSs@roundhouse.proxy.rlwy.net:43438",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb://mongo:OWherpCwRQMqFEOqNhDNXoxpbFRGUIPC@roundhouse.proxy.rlwy.net:44461",
   SESSION_NAME: process.env.SESSION_NAME || "IZUMI-MD-666~"
 };
